94c5fba2da1908df0d998e6be32a6c590ec7e1ff
Resister-boy <hassanpumped17@gmail.com>
2023-04-16 23:09:19 +0900

    Create Button Components Describe Home Page Message, Applicate framer motion

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,9 +1,11 @@
 import TwitterIcon from './TwitterIcon';
 import InstagramIcon from './InstagramIcon';
 import GithubIcon  from './GithubIcon';
+import LinkArrow from './LinkArrow';
 
 export {
   TwitterIcon,
   InstagramIcon,
-  GithubIcon
+  GithubIcon,
+  LinkArrow
 }
