449819ee712ae8340f84ad2e77bfcc38e85883d1
Resister-boy <hassanpumped17@gmail.com>
2023-04-25 03:17:12 +0900

    Create Project Card

---
+++ components/common/ProjectCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectCard = () => {
+  return (
+    <main className="mt-16 ml-4 h-auto border-gray-400 border-2 rounded-lg">
+      <div>
+        INFORMATION
+      </div>
+      <div>
+        CATEGORIES
+      </div>
+    </main>
+  )
+}
+
+export default ProjectCard
--- components/common/RenderBlock.tsx
+++ components/common/RenderBlock.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RenderBlock = (block: any) => {
   return (
-    <div className={`w-full ${renderBlockObject(block).background} text-[#333]`}>
+    <div className={`${renderBlockObject(block).background} text-[#333]`}>
       <p className={`${renderBlockObject(block).styles}`}>
         {renderBlockObject(block).content}
       </p>
